6b52f22304dc59c8a9b718b23d048d57243db9b1
ruranjo <rubens.rangel.job@gmail.com>
2023-07-01 07:50:33 -0400

    finish home section

---
+++ src/components/Home.jsx
@@ -0,0 +1,145 @@
+import React, { useState } from 'react'
+import hero from '../assets/hero.png'
+import styled from 'styled-components'
+
+export default function Home() {
+    const [value, setValue] = useState("$5000 - $10000")
+  return (
+    <Section>
+        <div className='background'> 
+            <img src={hero} alt="background" />
+        </div>
+        <div className="content">
+            <div className="info">
+                <h1>It's Time To </h1>
+                <h1>Explore The World</h1>
+                <button>Plan Your Trip</button>
+            </div>
+        </div>
+        <div className="planner">
+            <form action="">
+                <div className="row">
+                    <label>
+                        Destinations
+                    </label>
+                    <select>
+                        <option>arab Egyp</option>
+                        <option>caracas venezuela</option>
+                    </select>
+                </div>
+                <div className="row">
+                    <label>Check In</label>
+                    <input type="date" />
+                </div>
+                <div className="row">
+                    <label>
+                        Check in
+                    </label>
+                    <input type="text" value={value} onChange={e => {
+                        setValue(e.target.value)
+                    }}/>
+                </div>
+                <div className="row">
+                    <button>Discover More</button>
+                </div>
+            </form>
+        </div>
+    </Section>
+    
+   
+  )
+}
+
+const Section = styled.section`
+    margin-top:2rem;
+    position: relative;
+    .background{
+        img{
+            height:90vh;
+            width: 100%;
+        }
+    }
+    .content{
+        .info{
+            position:absolute;
+            top: 5rem;
+            margin-left: 8rem;
+            h1{
+                font-size: 5rem;
+                margin-bottom:2rem;
+            }
+        }
+    }
+
+    .planner{
+        position:absolute;
+        bottom: -2rem;
+        right: 0;
+        background-color:white;
+        padding:2rem;
+        box-shadow:rgba(100,100,111,0.2) 0px 7px 29px 0px;
+        form{
+            display:flex;
+            align-items: center;
+            justify-content: flex-end;
+            gap:6rem;
+            .row{
+                display:flex;
+                flex-direction: column;
+                text-align:start;
+                label{
+                    font-size: 0.7rem;
+                    color: var(--secondary-text);
+                }
+                input[type="date"]::-webkit-calendar-picker-indicator{
+                    cursor: pointer;
+                    filter: invert(58%) sepia(69%) saturate(2588%) hue-rotate(325deg) brightness(105%) contrast(101%);
+                }
+                input:focus {
+                    outline: none;
+                }
+                input,select {
+                    border: none;
+                    width: 100%;
+                    color: var(--primary-color);
+                    margin-top: 0.5rem;
+                    background-color: white;
+                    font-size: 1.1rem;
+                    border-bottom: 1px solid #f5ebe9;
+                    padding-bottom: 0.3rem;
+                }
+            }
+        }
+
+    }
+    @media screen and (min-width: 280px) and (max-width: 1080px) {
+        .background {
+          img {
+            height: 50vh;
+          }
+        }
+        .content {
+          .info {
+            margin-left: 2rem;
+            h1 {
+              font-size: 2rem;
+              margin-bottom: 1rem;
+            }
+          }
+          .planner {
+            position: initial;
+            margin: 2rem;
+            form {
+              align-items: flex-start;
+              flex-direction: column;
+            }
+          }
+        }
+      }
+`   
+/*
+  
+    
+  
+`;
+*/
